d8f98d9feef8d377142ac9ea46b50aaa7e5fd11e
tmhuyett <toddhuyett@gmail.com>
2021-04-30 12:05:57 -0400

    'd'

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,51 +7,56 @@ import Seo from "../components/seo"
 import { useEffect, useState } from "react"
 
 const IndexPage = () => {
+  const [data, setData] = useState([]) // new
 
-  const [data, setData] = useState([]); // new
+  useEffect(() => {
+    fetch("https://casarosada.herokuapp.com/flights")
+      .then(response => response.json())
+      .then(data => {
+        setData(data) // new
+      })
+  }, [])
 
-	useEffect(() => {
-		fetch("https://casarosada.herokuapp.com/flights")
-		.then((response) => response.json())
-		.then((data) => {
-			setData(data) // new
-		})
-}, [])
-
-return (
-  <Layout>
-    <Seo title="Home" />
-    <div>
-       <h4>Departures</h4>     
-       <div className="wrapper2">
-			       <div className="box2 box1">Airline</div>
-  <div className="box2 box2">Flight</div>
-  <div className="box2">To</div>
-  <div className="box2">Scheduled</div>
-  <div className="box2">Gate</div>
-         
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <div>
+        <h4>Departures</h4>
+        <div className="wrapper2">
+          <div className="box7">Airline</div>
+          <div className="box7">Flight</div>
+          <div className="box7">To</div>
+          <div className="box7">Scheduled</div>
+          <div className="box7">Gate</div>
         </div>
-        {data.map(item => (
-        <div className="wrapper" key={item.id}>
-			       <div class="box box3">{item.airline}</div>
-  <div className="box box4">{item.fnumber}</div>
-  <div className="box">{item.ato}</div>
-  <div className="box">{item.scheduled}</div>
-  <div className="box">{item.gate}</div>
-         
-        </div>
-      ))}
+        {data.map(item => {
+          if (item.id % 2 === 1) {
+            return (
+              <div className="wrapper" key={item.id}>
+                <div class="box8">{item.airline}</div>
+                <div className="box8">{item.fnumber}</div>
+                <div className="box8">{item.ato}</div>
+                <div className="box8">{item.scheduled}</div>
+                <div className="box8">{item.gate}</div>
+              </div>
+            )
+          } else {
+            return (
+              <div className="wrapper" key={item.id}>
+                <div class="box9">{item.airline}</div>
+                <div className="box9">{item.fnumber}</div>
+                <div className="box9">{item.ato}</div>
+                <div className="box9">{item.scheduled}</div>
+                <div className="box9">{item.gate}</div>
+              </div>
+            )
+          }
+        })}
+      </div>
 
-
-
-</div>
-   
-
-
-    <p>
-   
-    </p>
-  </Layout>
-)}
+      <p></p>
+    </Layout>
+  )
+}
 
 export default IndexPage
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,70 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { useEffect, useState } from "react"
 
-const SecondPage = () => (
+const SecondPage = () => {
+
+  const [data, setData] = useState([]); // new
+
+	useEffect(() => {
+		fetch("https://casarosada.herokuapp.com/flights")
+		.then((response) => response.json())
+		.then((data) => {
+			setData(data) // new
+		})
+}, [])
+
+return (
   <Layout>
-    <Seo title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Seo title="Home" />
+    <div>
+       <h4>Departures</h4>     
+       <div className="wrapper2">
+			       <div className="box7">Airline</div>
+  <div className="box7">Flight</div>
+  <div className="box7">To</div>
+  <div className="box7">Scheduled</div>
+  <div className="box7">Gate</div>
+         
+        </div>
+        {data.map(item => {
+          if(item.id%2===1) {
+          return (
+        <div className="wrapper" key={item.id}>
+			       <div class="box8">{item.airline}</div>
+  <div className="box8">{item.fnumber}</div>
+  <div className="box8">{item.ato}</div>
+  <div className="box8">{item.scheduled}</div>
+  <div className="box8">{item.gate}</div>
+         
+        </div>
+      )} else {
+        return( 
+          <div className="wrapper" key={item.id}>
+          <div class="box9">{item.airline}</div>
+<div className="box9">{item.fnumber}</div>
+<div className="box9">{item.ato}</div>
+<div className="box9">{item.scheduled}</div>
+<div className="box9">{item.gate}</div>
+      
+     </div>
+        )
+      }})}
+
+
+
+</div>
+   
+
+
+    <p>
+   
+    </p>
   </Layout>
-)
+)}
 
 export default SecondPage
